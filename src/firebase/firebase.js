2cc0dd340311dde22070f12792fe2476244981fa
Procheta Bhattacharyya <53190270+procheta1999@users.noreply.github.com>
2020-07-20 02:45:54 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,21 +3,21 @@ import "@firebase/auth";
 import "@firebase/database";
 
 const prodConfig = {
-  apiKey: "AIzaSyDfqpLjCdb9tCdbCIFEI90LkXeEfeBUzs4",
-  authDomain: "dsc-nsec-blogs.firebaseapp.com",
-  databaseURL: "https://dsc-nsec-blogs.firebaseio.com",
-  projectId: "dsc-nsec-blogs",
-  storageBucket: "dsc-nsec-blogs.appspot.com",
-  messagingSenderId: "1066927664407",
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
 };
 
 const devConfig = {
-  apiKey: "AIzaSyDfqpLjCdb9tCdbCIFEI90LkXeEfeBUzs4",
-  authDomain: "dsc-nsec-blogs.firebaseapp.com",
-  databaseURL: "https://dsc-nsec-blogs.firebaseio.com",
-  projectId: "dsc-nsec-blogs",
-  storageBucket: "dsc-nsec-blogs.appspot.com",
-  messagingSenderId: "1066927664407",
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
 };
 
 const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
